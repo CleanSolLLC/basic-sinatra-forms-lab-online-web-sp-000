e7c36870d6e2ab6057d66b87015d2dc3718067d8
Mark May <mamark90@gmail.com>
2020-08-30 02:43:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,15 +3,10 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
   get '/newteam' do
-    
     erb :newteam
   end
   
   post '/newteam' do
-    e
-  end
-  
-  get '/team' do
     raise params.inspect
     erb :team
   end
